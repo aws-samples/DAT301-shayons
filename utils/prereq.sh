f9c137841615e5e961407a62557cfe1714e89f68
Shayon Sanyal <shayons@amazon.com>
2024-11-13 16:56:02 -0500

    updates

--- utils/prereq.sh
+++ utils/prereq.sh
@@ -58,7 +58,7 @@ BEDROCK_CLAUDE_MODEL_ID=anthropic.claude-3-5-sonnet-20240620-v1:0
 BEDROCK_CLAUDE_MODEL_ARN=arn:aws:bedrock:us-west-2::foundation-model/anthropic.claude-3-5-sonnet-20240620-v1:0
 
 # Lambda configuration
-Note: Don't change this value
+# Note: Don't change this value
 LAMBDA_FUNCTION_NAME=genai-dat-301-labs_BedrockAgent_Lambda
 EOL
     
